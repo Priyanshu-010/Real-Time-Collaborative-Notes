5edc15a969f90043d4fabd0e859a915efc1b80e4
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-02-19 13:02:39 +0530

    Auth controller and routes created

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,21 @@ import dotenv from "dotenv"
 import http from "http"
 import { initSocket } from "./src/config/socket.js";
 import connectDb from "./src/config/db.js";
+import authRouter from "./src/routes/auth.route.js"
 
 const app = express()
 const server = http.createServer(app);
 
 initSocket(server)
+dotenv.config()
 
 app.use(cors())
 app.use(express.json())
-dotenv.config()
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-  res.send("Hello World")
-})
+app.use("/api/auth", authRouter)
+
 
 server.listen(PORT, ()=>{
   console.log(`Server is running on port ${PORT}`)
