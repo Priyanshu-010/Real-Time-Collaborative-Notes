205e8d331dba413f165719cb322532ca912786fe
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-02-20 12:01:19 +0530

    API and Auth Context created

---
+++ frontend/src/api/auth.api.js
@@ -0,0 +1,4 @@
+import axiosInstance from "./axios";
+
+export const registerUser = (data) => axiosInstance.post("/auth/register", data)
+export const loginUser = (data) => axiosInstance.post("/auth/login", data)
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,8 +1,62 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { loginUser, registerUser } from "../api/auth.api.js";
 
-function AuthContext() {
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setUser({ token });
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const { data } = await loginUser({ email, password });
+      localStorage.setItem("token", data.token);
+
+      setUser(data.user);
+
+      toast.success("Login successful");
+      navigate("/");
+    } catch (error) {
+      console.log("Error in login authContext: ", error);
+      toast.error("Login Failed");
+    }
+  };
+
+  const register = async (name, email, password) => {
+    try {
+      const { data } = await registerUser({ name, email, password });
+      localStorage.setItem("token", data.token);
+
+      setUser(data.user);
+
+      toast.success("Registration successful");
+      navigate("/");
+    } catch (error) {
+      console.log("Error in register authContext: ", error);
+      toast.error("Registration Failed");
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/login");
+  };
   return (
-    <div>AuthContext</div>
-  )
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export default AuthContext
+export default AuthContext;
