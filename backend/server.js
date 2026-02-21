de784b1f6a9d5eb9b0aa2f41ef4b46a2081be4f6
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-02-21 09:13:19 +0530

    Backend deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import http from "http"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
 import { initSocket } from "./src/config/socket.js";
 import connectDb from "./src/config/db.js";
-import authRouter from "./src/routes/auth.route.js"
-import noteRouter from "./src/routes/note.route.js"
-import versionRouter from "./src/routes/version.routes.js"
+import authRouter from "./src/routes/auth.route.js";
+import noteRouter from "./src/routes/note.route.js";
+import versionRouter from "./src/routes/version.routes.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 const server = http.createServer(app);
 
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+app.use(express.json());
 
-app.use("/api/auth", authRouter)
-app.use("/api/notes", noteRouter)
-app.use("/api/versions", versionRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/notes", noteRouter);
+app.use("/api/versions", versionRouter);
 
-initSocket(server)
+initSocket(server);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
-  connectDb()
-})
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDb();
+});
 
-export default app
+export default app;
